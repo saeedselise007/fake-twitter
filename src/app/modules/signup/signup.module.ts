e3fc0c7a2eb4a1701e00405db4d61f2cd51ab7a2
samisaeed <mahmud.saeed@selise.ch>
2023-08-06 05:12:02 +0600

    add

--- src/app/modules/signup/signup.module.ts
+++ src/app/modules/signup/signup.module.ts
@@ -7,8 +7,8 @@ import {FlexModule} from "@angular/flex-layout";
 import {MatCardModule} from "@angular/material/card";
 import {MatButtonModule} from "@angular/material/button";
 import {MatInputModule} from "@angular/material/input";
-import {MatFormFieldModule} from '@angular/material/form-field';
 import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+import {MatFormFieldModule} from '@angular/material/form-field';
 
 
 @NgModule({
@@ -24,7 +24,7 @@ import {FormsModule, ReactiveFormsModule} from "@angular/forms";
     MatButtonModule,
     MatInputModule,
     ReactiveFormsModule,
-    // FormsModule,
+    FormsModule,
 
   ]
 })
